b3539f79f6bcf187a0b647245b6f045c8a672b7a
NCC-Kai <kajetan.orski@norfolk.gov.uk>
2022-10-14 10:56:45 +0100

    simple setup, programatic link generation

--- public/scripts/utilities.js
+++ public/scripts/utilities.js
@@ -20,7 +20,14 @@
  * CONSTANTS
 ********************************************************************/
 
+const RESULTS_WRAPPER = document.querySelector('#page-content');
 
+const SEARCH_INDICATOR = document.querySelector('#search-indicator');
+const FILTER_INDICATOR = document.querySelector('#multi-filter-indicator');
+const RESULT_NUM_INDICATOR = document.querySelector('#result-number-indicator');
+
+const PAGE_LINKS = document.querySelector('#libraries-pagination-list');
+const MAX_EVENTS_PER_PAGE = 6;
 
 /********************************************************************
  * UTILITIES
@@ -28,20 +35,21 @@
 
 window.onload = function() {
   console.log('test');
+  calculatePageCount();
 }
 
 function setCookie(cname, cvalue, exdays) {
   const d = new Date();
   d.setTime(d.getTime() + (exdays*24*60*60*1000));
-  let expires = "expires="+ d.toUTCString();
-  document.cookie = cname + "=" + cvalue + ";" + expires + ";path=/";
+  let expires = 'expires='+ d.toUTCString();
+  document.cookie = cname + '=' + cvalue + ';' + expires + ';path=/';
 }
 
 function getCookie(cname) {
-  let name = cname + "=";
+  let name = cname + '=';
   let decodedCookie = decodeURIComponent(document.cookie);
   let ca = decodedCookie.split(';');
-  for(let i = 0; i <ca.length; i++) {
+  for (let i = 0; i < ca.length; i++) {
     let c = ca[i];
     while (c.charAt(0) == ' ') {
       c = c.substring(1);
@@ -50,7 +58,7 @@ function getCookie(cname) {
       return c.substring(name.length, c.length);
     }
   }
-  return "";
+  return '';
 }
 
 /********************************************************************
@@ -63,16 +71,32 @@ function getCookie(cname) {
  * PAGINATION
 ********************************************************************/
 
-const calculatePageCount = function(events) {
-
+const calculatePageCount = function() {
+  const events = RESULTS_WRAPPER.children.length;
+  console.log(`number of results= ${events}`);
+  const numPages = Math.ceil(events / MAX_EVENTS_PER_PAGE);
+  console.log(`number of pages= ${numPages}`);
+  generatePageLinks(numPages);
 }
 
 const displayEvents = function() {
 
 }
 
-const generatePageLinks = function() {
+// Takes the relevant number, returns the final element markup for a single link
+const generateLink = function(n) {
+  const newLink = document.createElement('li');
+  newLink.id = `page-${n}-link`;
+  newLink.classList.add('page-item');
+  newLink.innerHTML = `<button class="page-link" onclick="showPage(${n})">${n}</button>`;
+  return newLink;
+}
 
+// Iteratively generate the given number of page links
+const generatePageLinks = function(pageNum) {
+  for (i = 1; i <= pageNum; i++) {
+    PAGE_LINKS.appendChild(generateLink(i));
+  }
 }
 
 const nextPage = function() {
