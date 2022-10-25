f2ca0c78076d48c5d59015f47a12082cbf19d1a2
NCC-Kai <kajetan.orski@norfolk.gov.uk>
2022-10-25 18:24:16 +0100

    date format

--- public/scripts/utilities.js
+++ public/scripts/utilities.js
@@ -92,6 +92,49 @@ function fillFields() {
 
 const picker = document.querySelector('duet-date-picker');
 const output = document.querySelector('output');
+const DATE_FORMAT = /^(\d{1,2})-(\d{1,2})-(\d{4})$/;
+
+picker.dateAdapter = {
+  parse(value = "", createDate) {
+    const matches = value.match(DATE_FORMAT)
+    if (matches) {
+      return createDate(matches[3], matches[2], matches[1])
+    }
+  },
+  format(date) {
+    return `${date.getDate()}-${date.getMonth() + 1}-${date.getFullYear()}`
+  },
+}
+
+
+picker.localization = {
+  buttonLabel: "Choose date",
+  placeholder: "DD-MM-YYYY",
+  selectedDateMessage: "Selected date is",
+  prevMonthLabel: "Previous month",
+  nextMonthLabel: "Next month",
+  monthSelectLabel: "Month",
+  yearSelectLabel: "Year",
+  closeLabel: "Close window",
+  calendarHeading: "Choose a date",
+  dayNames: ["Sunday", "Monday", "Tuesday", "Wednesday", "Thursday", "Friday", "Saturday"],
+  monthNames: [
+    "January",
+    "February",
+    "March",
+    "April",
+    "May",
+    "June",
+    "July",
+    "August",
+    "September",
+    "October",
+    "November",
+    "December",
+  ],
+  monthNamesShort: ["Jan", "Feb", "Mar", "Apr", "May", "Jun", "Jul", "Aug", "Sep", "Oct", "Nov", "Dec"],
+  locale: "en-GB",
+}
 
 picker.addEventListener('duetChange', function(event) {
   console.log(event.detail.value);
